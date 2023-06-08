97c185406eeb343c94c1c839dbce19c83d964023
martin <martin_andanio@tokoonderdil.id>
2023-06-08 14:23:34 +0700

    update

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 http.listen(port, () => {
     console.log(`Listening on port ${port}`);
