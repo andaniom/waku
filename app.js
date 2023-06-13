12bed1b3feb3cb7562746db37647ec07acef4968
martin <martin_andanio@tokoonderdil.id>
2023-06-13 10:55:17 +0700

    update

--- app.js
+++ app.js
@@ -155,6 +155,7 @@ const initClient = function (clientId) {
     const client = new Client({
         restartOnAuthFail: true,
         puppeteer: {
+            headless: false,
             args: [
                 '--no-sandbox',
             ]
